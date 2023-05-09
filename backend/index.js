f7784dd6371f83aa038dc3cb90250baa5c4e1701
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-09 12:32:06 -0500

    I'm wondering now if vercel is the right solution

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRouter.js';
-import productRouter from './routes/productRouter.js';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
 dotenv.config();
 const PORT = process.env.PORT || 5100;
+
+const server = process.env.SERVER;
 
 const app = express();
 
@@ -22,12 +23,11 @@ app.use(cors({ credentials: true }));
 
 // Routes
 app.use('/users', userRouter);
-app.use('/products', productRouter);
 
 app.get('/', (req, res) => {
 	res.send('Welcome to the api...');
 });
 
 app.listen(PORT, () =>
-	console.log(`Server running on port: http://localhost:${PORT}`),
+	console.log(`Server running on port: ${server}:${PORT}`),
 );
