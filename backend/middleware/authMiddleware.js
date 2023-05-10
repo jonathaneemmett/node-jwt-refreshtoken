7c1785b3c9d96944eab5e74dec575d29c96ea4e9
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-10 13:38:08 -0500

    trying to figure out what happened in the middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,14 +10,17 @@ export async function tokenHandler(req, res, next) {
 	if (!context || !refreshToken)
 		return res
 			.status(401)
-			.json({ success: false, message: 'Unauthorized.' });
+			.json({
+				success: false,
+				message: 'No Context or RefreshToken, Unauthorized.',
+			});
 
 	// Get the auth token
 	const token = req.headers.authorization?.split(' ')[1];
 	if (!token)
 		return res
 			.status(401)
-			.json({ success: false, message: 'Unauthorized.' });
+			.json({ success: false, message: 'No Token, Unauthorized.' });
 
 	try {
 		// Verify the token
@@ -25,28 +28,40 @@ export async function tokenHandler(req, res, next) {
 		if (!decoded)
 			return res
 				.status(403)
-				.json({ success: false, message: 'Unauthorized.' });
+				.json({
+					success: false,
+					message: 'Token not verified, Unauthorized.',
+				});
 
 		// Get the user
 		const user = await User.findById(decoded.id);
 		if (!user)
 			return res
 				.status(401)
-				.json({ success: false, message: 'Unauthorized.' });
+				.json({
+					success: false,
+					message: 'User not found, Unauthorized.',
+				});
 
 		// Verify the refresh token
 		const decodedRefreshToken = await verifyRefreshToken(refreshToken);
 		if (!decodedRefreshToken)
 			return res
 				.status(401)
-				.json({ success: false, message: 'Unauthorized.' });
+				.json({
+					success: false,
+					message: 'Refresh Token not verified,Unauthorized.',
+				});
 
 		// Verify the context
 		const isMatch = await verifyContext(context, user.context);
 		if (!isMatch)
 			return res
 				.status(401)
-				.json({ success: false, message: 'Unauthorized.' });
+				.json({
+					success: false,
+					message: 'Context not verified, Unauthorized.',
+				});
 
 		// Set the user
 		req.user = user;
@@ -57,7 +72,10 @@ export async function tokenHandler(req, res, next) {
 		console.error(err);
 		return res
 			.status(401)
-			.json({ success: false, message: 'Unauthorized.' });
+			.json({
+				success: false,
+				message: 'Shit hit the fan, Unauthorized.',
+			});
 	}
 }
 
