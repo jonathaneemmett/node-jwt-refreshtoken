fe6551f1de6a7da785e1aa187acd700c68d62ea6
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-10 11:17:01 -0500

    admin and token refresh

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import argon2 from 'argon2';
 import User from '../models/User.js';
 import { getTokens } from '../utils/tokenGenerators.js';
+import { verifyRefreshToken, verifyContext } from '../utils/tokenGenerators.js';
 
 export async function register(req, res, next) {
 	const { email, password } = req.body;
@@ -151,5 +152,84 @@ export async function logout(req, res, next) {
 	res.status(200).json({
 		success: true,
 		message: 'User logged out.',
+	});
+}
+
+// Admin routes
+export async function getUsers(req, res, next) {
+	const users = await User.find({}).select('-password');
+
+	res.status(200).json(users);
+}
+
+// Token Routes
+export async function refreshToken(req, res, next) {
+	const { context, refreshToken } = req.cookies;
+
+	if (!context || !refreshToken)
+		return res
+			.status(401)
+			.json({ success: false, message: 'Unauthorized.' });
+
+	// Get the user
+	const decodedRefreshToken = await verifyRefreshToken(refreshToken);
+	if (!decodedRefreshToken)
+		return res
+			.status(401)
+			.json({ success: false, message: 'Unauthorized.' });
+
+	const user = await User.findById(decodedRefreshToken._id);
+	if (!user)
+		return res
+			.status(401)
+			.json({ success: false, message: 'Unauthorized.' });
+
+	// Verify the context
+	const isMatch = await verifyContext(context, user.context);
+	if (!isMatch)
+		return res
+			.status(401)
+			.json({ success: false, message: 'Unauthorized.' });
+
+	// Get tokens
+	const {
+		token,
+		refreshToken: newRefreshToken,
+		context: newContext,
+	} = await getTokens(user);
+
+	// Save context
+	user.context = newContext.context;
+
+	// Save user
+	await user.save();
+
+	// Put refresh token and context in a httpOnly cookie
+	res.cookie('refreshToken', newRefreshToken, {
+		path: '/',
+		httpOnly: true,
+		sameSite: 'strict',
+		secure: process.env.NODE_ENV === 'production',
+		maxAge: 60 * 60 * 24 * 1, // 1 day
+	});
+
+	res.cookie('context', newContext.contextToken, {
+		path: '/',
+		httpOnly: true,
+		sameSite: 'strict',
+		secure: process.env.NODE_ENV === 'production',
+		maxAge: 60 * 60 * 24 * 1, // 1 day
+	});
+
+	// Send response
+	res.status(201).json({
+		success: true,
+		message: 'User logged in.',
+		token,
+		user: {
+			id: user._id,
+			email: user.email,
+			role: user.role,
+		},
 	});
 }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,23 @@
 import { Router } from 'express';
+import { tokenHandler, adminHandler } from '../middleware/authMiddleware.js';
 
-import { register, login, logout } from '../controllers/userController.js';
+import {
+	register,
+	login,
+	logout,
+	getUsers,
+	refreshToken,
+} from '../controllers/userController.js';
 
 const router = Router();
 
+// Protected routes
+router.get('/', [tokenHandler, adminHandler], getUsers);
+
+// Public routes
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
+router.post('/refresh', refreshToken);
 
 export default router;
